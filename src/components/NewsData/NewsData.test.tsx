78481329e80a533d080ee57636959e75f948f469
Mo Jama <23492964+mokyox@users.noreply.github.com>
2021-04-30 21:42:20 +0100

    update unit test for news data to use getByText selector and update ESLint config for prettier

--- src/components/NewsData/NewsData.test.tsx
+++ src/components/NewsData/NewsData.test.tsx
@@ -10,18 +10,19 @@ test("renders a <Card/> correctly with given props", () => {
       id: "Exotic Weapons",
       title: "Exotic Weapons",
       tabTitle: null,
+      src: "",
       body:
         "Got Bars? Some of the Island's Characters have exotic weapons that they'll part with only for a hefty price. Track them down and try one out today!",
       image:
-        "https://cdn2.unrealengine.com/15br-exoticweapons-motd-1920x1080-1920x1080-814e0e55efa8.jpg",
+        "https://cdn-live.prm.ol.epicgames.com/15br-exoticweapons-motd-1920x1080-1920x1080-814e0e55efa8.jpg",
       tileImage:
-        "https://cdn2.unrealengine.com/15br-exoticweapons-motd-1024x512-1024x512-d6ff496883b8.jpg",
+        "https://cdn-live.prm.ol.epicgames.com/15br-exoticweapons-motd-1024x512-1024x512-d6ff496883b8.jpg",
       sortingPriority: 10,
       hidden: false,
     },
   ];
   render(<NewsData data={mockNewsData} />);
 
-  const mockCard = screen.getByTestId("news-data");
-  expect(mockCard).toBeInTheDocument();
+  const mockCardTitle = screen.getByText("Exotic Weapons");
+  expect(mockCardTitle).toBeInTheDocument();
 });
